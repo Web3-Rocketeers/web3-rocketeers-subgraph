a3e0196d594966514f8e48bcb7e884780ddd38d5
Fonck <AFonck@protonmail.com>
2023-03-31 17:00:15 -0600

    Subgraph created & types generated succesfuly

--- src/mappings/GovernanceToken.ts
+++ src/mappings/GovernanceToken.ts
@@ -1,24 +1,34 @@
-import { Transfer } from "../generated/GovernanceToken/ERC20";
+import { TransferSingle } from "../generated/GovernanceToken/Web3Rocketeers";
 import { TokenHolder } from "../generated/schema";
 
-export function handleTransfer(event: Transfer): void {
-  let from = event.params.from.toHex();
-  let to = event.params.to.toHex();
-  let value = event.params.value;
+// Update GOVERNANCE_TOKEN_ID to match the ID of your governance token
+const GOVERNANCE_TOKEN_ID = "256";
 
-  let fromTokenHolder = TokenHolder.load(from);
-  if (fromTokenHolder == null) {
-    fromTokenHolder = new TokenHolder(from);
-    fromTokenHolder.balance = BigInt.fromI32(0);
-  }
-  fromTokenHolder.balance = fromTokenHolder.balance.minus(value);
-  fromTokenHolder.save();
+export function handleTransferSingle(event: TransferSingle): void {
+  let tokenId = event.params.id.toHex();
 
-  let toTokenHolder = TokenHolder.load(to);
-  if (toTokenHolder == null) {
-    toTokenHolder = new TokenHolder(to);
-    toTokenHolder.balance = BigInt.fromI32(0);
+  // Only track transfers for the governance token
+  if (tokenId === GOVERNANCE_TOKEN_ID) {
+    let from = event.params.from.toHex();
+    let to = event.params.to.toHex();
+    let value = event.params.value;
+
+    // Update the balance for the 'from' TokenHolder
+    let fromTokenHolder = TokenHolder.load(from);
+    if (fromTokenHolder == null) {
+      fromTokenHolder = new TokenHolder(from);
+      fromTokenHolder.balance = BigInt.fromI32(0);
+    }
+    fromTokenHolder.balance = fromTokenHolder.balance.minus(value);
+    fromTokenHolder.save();
+
+    // Update the balance for the 'to' TokenHolder
+    let toTokenHolder = TokenHolder.load(to);
+    if (toTokenHolder == null) {
+      toTokenHolder = new TokenHolder(to);
+      toTokenHolder.balance = BigInt.fromI32(0);
+    }
+    toTokenHolder.balance = toTokenHolder.balance.plus(value);
+    toTokenHolder.save();
   }
-  toTokenHolder.balance = toTokenHolder.balance.plus(value);
-  toTokenHolder.save();
 }
